205cd9ba64827c043769e3e313d2939e18d6fe1c
BRalan22 <githubcursoti@gmail.com>
2026-04-08 15:31:50 -0300

    Delete db.js

--- db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = function() {
-    
-    mongoose.connect('mongodb://localhost:27017/cine_db')
-        .then(() => console.log(' Conectado ao MongoDB (CineDB)...'))
-        .catch(err => console.error(' Erro ao conectar ao MongoDB:', err));
-}
